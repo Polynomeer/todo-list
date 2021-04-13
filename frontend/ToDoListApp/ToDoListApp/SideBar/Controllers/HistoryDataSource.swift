5030b28bea48a532054db4ae05a8719f92b0af79
dahun-lee-daji <ssss6596@gmail.com>
2021-04-13 19:02:01 +0900

    Feature : Create HistoryCell, HistoryDatasource (#7)

---
+++ frontend/ToDoListApp/ToDoListApp/SideBar/Controllers/HistoryDataSource.swift
@@ -0,0 +1,23 @@
+//
+//  HistoryDataSource.swift
+//  ToDoListApp
+//
+//  Created by 이다훈 on 2021/04/13.
+//
+
+import UIKit
+
+class HistoryDatasource: NSObject, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "HistoryCell", for: indexPath) as? HistoryCell else {
+            return UITableViewCell()
+        }
+        cell.historyContent.text = "content \(indexPath.row)"
+        cell.historyDate.text = "\(Date.init())"
+        return cell
+    }
+}
