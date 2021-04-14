aaae9ab06b20f65914a2b634679d15710e40dc43
dahun-lee-daji <ssss6596@gmail.com>
2021-04-15 00:48:43 +0900

    Feature : Insert DB history data into the history cell

--- frontend/ToDoListApp/ToDoListApp/SideBar/Controllers/HistoryDataSource.swift
+++ frontend/ToDoListApp/ToDoListApp/SideBar/Controllers/HistoryDataSource.swift
@@ -26,8 +26,49 @@ class HistoryDatasource: NSObject, UITableViewDataSource {
         }
         let sortedHistoryData = positionManager.sort(history: historyData)
         
-        cell.historyContent.text = "\(sortedHistoryData[indexPath.row].author) \(sortedHistoryData[indexPath.row].title)"
-        cell.historyDate.text = "\(sortedHistoryData[indexPath.row].date)"
+        cell.historyContent.text = createHistoryMessage(with: sortedHistoryData[indexPath.row]).show()
+        cell.historyDate.text = timelapseMessage(with: sortedHistoryData[indexPath.row].date)
         return cell
+    }
+    
+    private func createHistoryMessage(with data : HistoryData) -> HistoryMessage {
+        switch data.action {
+        case "move":
+            return moveMessage.init(title: data.title, from: data.from!, to: data.to!)
+        case "add":
+            return addMessage.init(title: data.title, to: data.to!)
+        case "remove":
+            return removeMessage.init(title: data.title, to: data.to!)
+        case "update":
+            return updateMessage.init(title: data.title, to: data.to!)
+        default:
+            return nilMessage()
+        }
+        
+    }
+    
+    private func timelapseMessage(with dateString : String) -> String {
+        guard let date = dateString.toDate() else {
+            return "알 수 없음"
+        }
+        let min = 60
+        let hour = min * 60
+        let day = hour * 24
+        
+        let timeGap = abs(Int(date.timeIntervalSince(Date())))
+        
+        if timeGap / day > 0 {
+            return "\(timeGap / day)일 전"
+        }
+        
+        if timeGap / hour > 0 {
+            return "\(timeGap / hour)시간 전"
+        }
+        
+        if timeGap / min > 0 {
+            return "\(timeGap / min)분 전"
+        }
+        
+        return "방금 전"
     }
 }
